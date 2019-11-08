2f7d19cfdb5fa38b4e0693ccb4984ed5f2e8e3db
CubeRuben <egor.petrorov.03@mail.ru>
2019-11-09 01:33:00 +0300

    Fast Fix

---
+++ RPPlugin/RPPlugin/Scripts/IronBlyat.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPPlugin.Scripts
+{
+    public class IronBlyat : MonoBehaviour
+    {
+        NavMeshAgent agent;
+        Scp939PlayerScript[] players;
+
+
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            players = GameObject.FindObjectsOfType<Scp939PlayerScript>();
+        }
+
+        void FixedUpdate()
+        {
+            float distance = Vector3.Distance(players[1].gameObject.transform.position, transform.position);
+
+            if (distance <= 10)
+            {
+                agent.SetDestination(players[1].gameObject.transform.position);
+                
+            }
+            //transform.position += new Vector3(0, 0.1f, 0);
+        }
+
+    }
+}
